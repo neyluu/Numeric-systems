2105d5e2a04081fdb152887791084cecf16ba2e4
neyluu <dwa2321@gmail.com>
2023-06-14 21:08:07 +0200

    calculator new version

---
+++ js/calculatorModeSwitch.js
@@ -0,0 +1,24 @@
+const calculatorButton = document.querySelector(".calc")
+const converterButton = document.querySelector(".convert")
+const backgroundLeft = document.querySelector(".option-cal")
+const backgroundRight = document.querySelector(".option-con")
+
+
+
+
+
+// if(document.querySelector("#calculator").checked)
+// {
+//      backgroundLeft.style.backgroundColor = "#52734D";
+// }
+
+// calculatorButton.addEventListener("click", () => {
+//     backgroundLeft.style.backgroundColor = "#52734D";
+//     backgroundRight.style.backgroundColor = "#91C788"
+// })
+// converterButton.addEventListener("click", () => {
+//     backgroundLeft.style.backgroundColor = "#91C788"
+//     backgroundRight.style.backgroundColor = "#52734D";
+// })
+
+
